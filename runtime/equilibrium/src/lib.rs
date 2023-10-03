457828124739fe9f889d1c256d1800a56e4b36e4
Maks Nabokov <max.nab@equilibrium.io>
2023-10-03 17:04:12 +0300

    bump spec version

--- runtime/equilibrium/src/lib.rs
+++ runtime/equilibrium/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("Equilibrium-parachain"),
     impl_name: create_runtime_str!("Equilibrium-parachain"),
     authoring_version: 10,
-    spec_version: 33,
+    spec_version: 34,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
