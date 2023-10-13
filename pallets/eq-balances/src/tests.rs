7192f4f1bdd16a2076c3b0ad74822325d6cf5140
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2023-10-13 15:19:22 +0300

    Implement cDOTs swap

--- pallets/eq-balances/src/tests.rs
+++ pallets/eq-balances/src/tests.rs
@@ -1856,29 +1856,40 @@ fn locked_balance_remove_lock() {
 }
 
 #[test]
-fn allow_xdots_swap() {
+fn allow_crowdloan_dots_swap() {
     new_test_ext().execute_with(|| {
-        assert_ok!(ModuleBalances::allow_xdots_swap(
+        assert_ok!(ModuleBalances::allow_crowdloan_swap(
             RawOrigin::Root.into(),
-            vec![XDotAsset::XDOT, XDotAsset::XDOT3]
+            vec![
+                CrowdloanDotAsset::XDOT,
+                CrowdloanDotAsset::XDOT3,
+                CrowdloanDotAsset::CDOT714
+            ]
         ));
 
         assert_eq!(
-            AllowedXdotsSwap::<Test>::get(),
-            vec![XDotAsset::XDOT, XDotAsset::XDOT3]
+            AllowedCrowdloanDotsSwap::<Test>::get(),
+            vec![
+                CrowdloanDotAsset::XDOT,
+                CrowdloanDotAsset::XDOT3,
+                CrowdloanDotAsset::CDOT714
+            ]
         );
 
-        assert_ok!(ModuleBalances::allow_xdots_swap(
+        assert_ok!(ModuleBalances::allow_crowdloan_swap(
             RawOrigin::Root.into(),
-            vec![XDotAsset::XDOT2]
+            vec![CrowdloanDotAsset::XDOT2]
         ));
 
-        assert_eq!(AllowedXdotsSwap::<Test>::get(), vec![XDotAsset::XDOT2]);
+        assert_eq!(
+            AllowedCrowdloanDotsSwap::<Test>::get(),
+            vec![CrowdloanDotAsset::XDOT2]
+        );
     });
 }
 
 #[test]
-fn swap_xdot() {
+fn swap_crowdloan_dots() {
     new_test_ext().execute_with(|| {
         let account_1: u64 = 1;
         let account_2: u64 = 2;
@@ -1902,6 +1913,16 @@ fn swap_xdot() {
             XDOT3,
             SignedBalance::Positive(215 * ONE_TOKEN),
         );
+        ModuleBalances::make_free_balance_be(
+            &account_1,
+            CDOT714,
+            SignedBalance::Positive(327 * ONE_TOKEN),
+        );
+        ModuleBalances::make_free_balance_be(
+            &account_1,
+            CDOT815,
+            SignedBalance::Positive(427 * ONE_TOKEN),
+        );
 
         // deposit main account_2
         ModuleBalances::make_free_balance_be(
@@ -1909,12 +1930,22 @@ fn swap_xdot() {
             XDOT,
             SignedBalance::Positive(40 * ONE_TOKEN),
         );
+        ModuleBalances::make_free_balance_be(
+            &account_2,
+            CDOT714,
+            SignedBalance::Positive(50 * ONE_TOKEN),
+        );
+        ModuleBalances::make_free_balance_be(
+            &account_2,
+            CDOT815,
+            SignedBalance::Positive(60 * ONE_TOKEN),
+        );
 
         // create debt for account_1_subaccount
         ModuleBalances::make_free_balance_be(
             &account_1_sub_account,
             DOT,
-            SignedBalance::Positive((781 + 782 + 783) * ONE_TOKEN),
+            SignedBalance::Positive((781 + 782 + 783 + 784 + 785) * ONE_TOKEN),
         );
         ModuleBalances::make_free_balance_be(
             &account_1_sub_account,
@@ -1931,56 +1962,129 @@ fn swap_xdot() {
             XDOT3,
             SignedBalance::Negative(783 * ONE_TOKEN),
         );
+        ModuleBalances::make_free_balance_be(
+            &account_1_sub_account,
+            CDOT714,
+            SignedBalance::Negative(784 * ONE_TOKEN),
+        );
+        ModuleBalances::make_free_balance_be(
+            &account_1_sub_account,
+            CDOT815,
+            SignedBalance::Negative(785 * ONE_TOKEN),
+        );
 
-        assert_ok!(ModuleBalances::allow_xdots_swap(
+        assert_ok!(ModuleBalances::allow_crowdloan_swap(
             RawOrigin::Root.into(),
-            vec![XDotAsset::XDOT, XDotAsset::XDOT3],
+            vec![
+                CrowdloanDotAsset::XDOT,
+                CrowdloanDotAsset::XDOT3,
+                CrowdloanDotAsset::CDOT714,
+                CrowdloanDotAsset::CDOT815
+            ],
         ));
 
         assert_err!(
-            ModuleBalances::swap_xdot(Some(account_1).into(), None, vec![XDotAsset::XDOT2]),
-            Error::<Test>::XDotSwapNotAllowed
+            ModuleBalances::swap_crowdloan_dots(
+                Some(account_1).into(),
+                None,
+                vec![CrowdloanDotAsset::XDOT2]
+            ),
+            Error::<Test>::CrowdloanDotSwapNotAllowed
         );
 
-        // main account_1 XDOT balances before swap
+        // main account_1 Crowdloan DOT balances before swap
         assert_balance!(&account_1, 40 * ONE_TOKEN, 0, XDOT);
         assert_balance!(&account_1, 100 * ONE_TOKEN, 0, XDOT2);
         assert_balance!(&account_1, 215 * ONE_TOKEN, 0, XDOT3);
+        assert_balance!(&account_1, 327 * ONE_TOKEN, 0, CDOT714);
+        assert_balance!(&account_1, 427 * ONE_TOKEN, 0, CDOT815);
 
-        // main account_2 XDOT balances before swap
+        // main account_2 Crowdloan DOT balances before swap
         assert_balance!(&account_2, 40 * ONE_TOKEN, 0, XDOT);
+        assert_balance!(&account_2, 50 * ONE_TOKEN, 0, CDOT714);
+        assert_balance!(&account_2, 60 * ONE_TOKEN, 0, CDOT815);
 
-        // account_1_subaccount XDOT balances before swap
+        // account_1_subaccount Crowdloan DOT balances before swap
         assert_balance!(&account_1_sub_account, 0, 781 * ONE_TOKEN, XDOT);
         assert_balance!(&account_1_sub_account, 0, 782 * ONE_TOKEN, XDOT2);
         assert_balance!(&account_1_sub_account, 0, 783 * ONE_TOKEN, XDOT3);
+        assert_balance!(&account_1_sub_account, 0, 784 * ONE_TOKEN, CDOT714);
+        assert_balance!(&account_1_sub_account, 0, 785 * ONE_TOKEN, CDOT815);
 
-        assert_ok!(ModuleBalances::swap_xdot(
+        assert_ok!(ModuleBalances::swap_crowdloan_dots(
             Some(account_1).into(),
             None,
-            vec![XDotAsset::XDOT, XDotAsset::XDOT3]
+            vec![
+                CrowdloanDotAsset::XDOT,
+                CrowdloanDotAsset::XDOT3,
+                CrowdloanDotAsset::CDOT714,
+                CrowdloanDotAsset::CDOT815
+            ]
         ));
 
-        assert_ok!(ModuleBalances::swap_xdot(
+        let mut swapped_cdots = VecMap::new();
+        swapped_cdots.insert(
+            CDOT714,
+            Balance::zero()
+                .checked_add((327 + 784) * ONE_TOKEN)
+                .unwrap(),
+        );
+        swapped_cdots.insert(
+            CDOT815,
+            Balance::zero()
+                .checked_add((427 + 785) * ONE_TOKEN)
+                .unwrap(),
+        );
+
+        assert_eq!(SwappedCDotAmounts::<Test>::get(), swapped_cdots);
+
+        assert_ok!(ModuleBalances::swap_crowdloan_dots(
             Some(account_1).into(),
             Some(account_2),
-            vec![XDotAsset::XDOT, XDotAsset::XDOT3]
+            vec![
+                CrowdloanDotAsset::XDOT,
+                CrowdloanDotAsset::XDOT3,
+                CrowdloanDotAsset::CDOT714,
+                CrowdloanDotAsset::CDOT815
+            ]
         ));
 
-        // main account_1 XDOT balances after swap
+        let mut swapped_cdots = VecMap::new();
+        swapped_cdots.insert(
+            CDOT714,
+            Balance::zero()
+                .checked_add((327 + 784 + 50) * ONE_TOKEN)
+                .unwrap(),
+        );
+        swapped_cdots.insert(
+            CDOT815,
+            Balance::zero()
+                .checked_add((427 + 785 + 60) * ONE_TOKEN)
+                .unwrap(),
+        );
+
+        assert_eq!(SwappedCDotAmounts::<Test>::get(), swapped_cdots);
+
+        // main account_1 Crowdloan DOT balances after swap
         assert_balance!(&account_1, 0, 0, XDOT);
         assert_balance!(&account_1, 100 * ONE_TOKEN, 0, XDOT2);
         assert_balance!(&account_1, 0, 0, XDOT3);
-        assert_balance!(&account_1, 255 * ONE_TOKEN, 0, DOT);
+        assert_balance!(&account_1, 0, 0, CDOT714);
+        assert_balance!(&account_1, 0, 0, CDOT815);
+        assert_balance!(&account_1, (40 + 215 + 327 + 427) * ONE_TOKEN, 0, DOT);
 
-        // main account_2 XDOT balances after swap
+        // main account_2 Crowdloan DOT balances after swap
         assert_balance!(&account_2, 0, 0, XDOT);
-        assert_balance!(&account_2, 40 * ONE_TOKEN, 0, DOT);
+        assert_balance!(&account_2, 0, 0, CDOT714);
+        assert_balance!(&account_2, 0, 0, CDOT815);
+        assert_balance!(&account_2, (40 + 50 + 60) * ONE_TOKEN, 0, DOT);
 
-        // account_1_subaccount XDOT balances after swap
+        // account_1_subaccount Crowdloan DOT balances after swap
         assert_balance!(&account_1_sub_account, 0, 0, XDOT);
         assert_balance!(&account_1_sub_account, 0, 782 * ONE_TOKEN, XDOT2);
         assert_balance!(&account_1_sub_account, 0, 0, XDOT3);
+        assert_balance!(&account_1_sub_account, 0, 0, CDOT714);
+        assert_balance!(&account_1_sub_account, 0, 0, CDOT815);
         assert_balance!(&account_1_sub_account, (782) * ONE_TOKEN, 0, DOT);
     });
 }
