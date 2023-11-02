8b2750b4efad8caa76f7ca2ac869b73a01eb9e5f
optifat <pavsmel@mail.ru>
2023-11-02 12:23:06 +0300

    eq-staking on_initialize remove locks (#12)
    eq-staking on_initialize remove locks

--- runtime/equilibrium/src/lib.rs
+++ runtime/equilibrium/src/lib.rs
@@ -2477,6 +2477,7 @@ parameter_types! {
     pub const EqStakingModuleId: PalletId = PalletId(*b"eq/stkng");
     pub const RewardsLockPeriod: eq_staking::StakePeriod = eq_staking::StakePeriod::Six;
     pub const MaxRewardExternalIdsCount: u32 = 1000;
+    pub const AccountsPerBlock: u32 = 100;
 }
 
 pub struct LiquidityAccount;
@@ -2506,6 +2507,7 @@ impl eq_staking::Config for Runtime {
     type LiquidityAccountCustom = LiquidityAccountCustom;
     type RewardsLockPeriod = RewardsLockPeriod;
     type MaxRewardExternalIdsCount = MaxRewardExternalIdsCount;
+    type AccountsPerBlock = AccountsPerBlock;
     type WeightInfo = ();
 }
 
